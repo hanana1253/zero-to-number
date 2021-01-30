07912bea10e3248b215b0b0c9ef26ff464fcacc4
hanana1253 <hanana1253@gmail.com>
2021-01-30 17:50:46 +0900

    feat: Add print function
    to print

--- zero-to-number.py
+++ zero-to-number.py
@@ -2,7 +2,7 @@
 
 
 # Print prompt message to get the number from user and save the value into input_num variable.
-input_num = int(input('Please enter the number (1 ~ 10) : '))
+input_num = int(input("Please enter the number (1 ~ 10) : "))
 
 
 result = ('No calculation yet')
